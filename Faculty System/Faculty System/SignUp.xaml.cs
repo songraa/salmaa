45eb5103e26fdbeb7c45680252fd7fc59a193f16
songraa <salma.0523076@gmail.com>
2024-11-13 15:44:35 +0200

    Add files via upload

---
+++ Faculty System/Faculty System/SignUp.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Faculty_System
+{
+    /// <summary>
+    /// Interaction logic for SignUp.xaml
+    /// </summary>
+    public partial class SignUp : Page
+    {
+        FacultyEntities1 fe = new FacultyEntities1();
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUpBS_Click(object sender, RoutedEventArgs e)
+        {
+
+            var recordData = fe.students.FirstOrDefault(x => x.StudentName == SignUpName.Text && x.Email == SignUpEmail.Text && x.Pass == SignUpPass.Text);
+            if(SignUpCoPass.Text!= SignUpPass.Text)
+            {
+                MessageBox.Show("Confirm Pass must match the pass");
+                return;
+            }
+            if (recordData != null)
+            {
+                MessageBox.Show("you already signed up, try to log in");
+            }
+            else
+            {
+                student student = new student
+                {
+                    StudentName = SignUpName.Text,
+                    Email = SignUpEmail.Text,
+                    Pass = SignUpPass.Text,
+                };
+                fe.students.Add(student);
+                fe.SaveChanges();
+                MessageBox.Show("Sign up successed!");
+            }
+
+            LogIn logIn = new LogIn();
+            this.NavigationService.Navigate(logIn);
+        }
+    }
+}
